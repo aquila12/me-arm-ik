221a315b861c2a56f28889202dcf0865a42ec795
Nick Moriarty <njmoriarty@gmail.com>
2014-05-01 19:23:50 +0100

    Check L1, L2; fix L3 based on v0.3 of the design.

--- ik.cpp
+++ ik.cpp
@@ -4,7 +4,7 @@
 const float PI=3.14159265359;
 float L1=80; //Shoulder to elbow length
 float L2=80; //Elbow to wrise length
-float L3=50; //Length from wrist to hand PLUS base centre to shoulder
+float L3=68; //Length from wrist to hand PLUS base centre to shoulder
 
 // Get polar coords from cartesian ones
 void cart2polar(float a, float b, float& r, float& theta)
